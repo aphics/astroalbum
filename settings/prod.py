1d1a81439aad96368c64648b3d2cdc1a8674e958
aphics <aphics@gmail.com>
2023-03-02 17:04:06 -0600

    Modificacion settings produccion

--- settings/prod.py
+++ settings/prod.py
@@ -1,13 +1,27 @@
 from .base import *
-import os
+import pymysql
+
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = []
+
+
+# Database
+# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+
+pymysql.version_info = (1, 4, 2, 'final', 0)
+
+pymysql.install_as_MySQLdb()
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ["DATABASE_NAME"],
-        'USER': os.environ["DATABASE_USER"],
-        'PASSWORD': os.environ["DATABASE_PASSWORD"],
-        'HOST': os.environ["DATABASE_HOST"],
-        'PORT': os.environ["DATABASE_PORT"],
+        'NAME': 'name_DB',
+        'USER': 'user_DB',
+        'PASSWORD': 'password_DB',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
